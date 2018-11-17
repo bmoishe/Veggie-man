c16dfea36b4b9e2b1cb044a0846a69befd72a9b6
bmoishe <moishebarnes@gmail.com>
2018-11-17 08:42:02 +0000

    first commit

---
+++ veggie-man.js
@@ -0,0 +1,77 @@
+function VeggieMan() {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed =0;
+  // frameRate(10);
+  this.total = 0;
+  this.tail = [];
+  document.getElementById("score").innerHTML = this.total;
+
+
+  this.dir = function(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+  this.update = function() {
+
+    // if (this.total === this.tail.length) {
+    //   for (var i = 0; i < this.tail.length-1; i++) {
+    //     this.tail[i] = this.tail[i+1];
+    //   }
+    // }
+    this.tail[this.total-1] = createVector(this.x, this.y);
+    this.x = this.x + this.xspeed*scle;
+    this.y = this.y + this.yspeed*scle;
+
+    this.x = constrain(this.x, 0, width-scle)
+    this.y = constrain(this.y, 0, height-scle)
+    document.getElementById("score").innerHTML = this.total;
+
+  }
+
+  this.eat = function(pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 1) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+    // this.eatMeat = function(pos) {
+    //   var d = dist(this.x, this.y, pos.x, pos.y);
+    //   if (d < 1) {
+    //     this.total--;
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+  }
+// also need to pass if food === meat then this.total--
+  this.death = function() {
+    for (var i = 0; i < this.tail.length; i++) {
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+      if (d < 1 ) {
+        this.total = 0
+        this.tail = []
+        setup();
+      }
+    }
+  }
+  this.show = function() {
+    fill(255);
+    for (var i = 0; i < this.tail.length-1; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scle, scle);
+    }
+
+
+    // for (var i = 0; i < this.total; i++) {
+    //   rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    // }
+    fill(255);
+    rect(this.x, this.y, scle, scle);
+
+  }
+}
